f0902a02224c09bb040bce7e953018800b808ded
Wahid Sabbir <137522117+w-sabbir-p@users.noreply.github.com>
2023-10-04 03:54:20 +0600

    Update main.dart

--- main.dart
+++ main.dart
@@ -3,6 +3,9 @@ import 'package:portfolio/Fragment/Skills.dart';
 import 'package:portfolio/Fragment/Experiences.dart';
 import 'package:portfolio/Fragment/About.dart';
 import 'package:portfolio/Fragment/Certificates.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -57,19 +60,19 @@ class HomeActivity extends StatelessWidget {
             isScrollable: true,
             tabs: [
               Tab(
-                icon: Icon(Icons.person, color: Colors.teal),
+                icon: Icon(Icons.person, color: Colors.tealAccent),
                 text: 'About',
               ),
               Tab(
-                icon: Icon(Icons.star, color: Colors.teal),
+                icon: Icon(Icons.star, color: Colors.tealAccent),
                 text: 'Skills',
               ),
               Tab(
-                icon: Icon(Icons.work, color: Colors.teal),
+                icon: Icon(Icons.work, color: Colors.tealAccent),
                 text: 'Experiences',
               ),
               Tab(
-                icon: Icon(Icons.card_membership, color: Colors.teal),
+                icon: Icon(Icons.card_membership, color: Colors.tealAccent),
                 text: 'Certificates',
               ),
             ],
@@ -77,33 +80,32 @@ class HomeActivity extends StatelessWidget {
         ),
         drawer: Drawer(
           child: Container(
-            color: Colors.black54, // Set the background color of the drawer to black
+            color: Colors.black54,
             child: ListView(
               padding: EdgeInsets.zero,
               children: [
                 DrawerHeader(
-                  padding: EdgeInsets.all(0),
-                  decoration: BoxDecoration(
-                    color: Colors.black,
+                  decoration: const BoxDecoration(
+                    color: Colors.black54,
                   ),
                   child: UserAccountsDrawerHeader(
-                    accountName: Text(
+                    accountName: const Text(
                       "Wahid Sabbir",
                       style: TextStyle(
-                        fontSize: 24,
+                        fontSize: 18,
                         fontWeight: FontWeight.bold,
                         color: Colors.white,
                       ),
                     ),
-                    accountEmail: Text(
+                    accountEmail: const Text(
                       "wahid15-3975@diu.edu.bd",
                       style: TextStyle(
-                        fontSize: 18,
+                        fontSize: 15,
                         color: Colors.white,
                       ),
                     ),
                     currentAccountPicture: ClipRRect(
-                      borderRadius: BorderRadius.circular(40),
+                      borderRadius: BorderRadius.circular(50),
                       child: Image.network(
                         "https://media.licdn.com/dms/image/D5603AQFgIjUpihMMvA/profile-displayphoto-shrink_400_400/0/1691085262924?e=1701302400&v=beta&t=59Or-RojKMYIQQZt5z7qlkPmzu-QZca8OHoH3w6sxpw",
                       ),
@@ -114,26 +116,140 @@ class HomeActivity extends StatelessWidget {
                   ),
                 ),
                 ListTile(
-                  leading: Icon(Icons.home),
+                  leading: Icon(
+                    Icons.home,
+                    color: Colors.tealAccent,
+                  ),
                   title: Text(
                     'Home',
-                    style: Theme.of(context).textTheme.subtitle2!.copyWith(color: Colors.white), // Set text color to white
+                    style: TextStyle(
+                      color: Colors.white, fontSize: 18,
+                    ),
                   ),
                   onTap: () {
-                    // Handle Home navigation here
+
+                  },
+                ),
+
+                ListTile(
+                  leading: Icon(
+                    Icons.settings,
+                    color: Colors.tealAccent,
+                  ),
+                  title: Text(
+                    'Settings',
+                    style: TextStyle(
+                      color: Colors.white, fontSize: 18,
+                    ),
+                  ),
+                  onTap: () {
+
                   },
                 ),
                 ListTile(
-                  leading: Icon(Icons.settings),
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.tealAccent,
+                  ),
                   title: Text(
-                    'Settings',
-                    style: Theme.of(context).textTheme.subtitle2!.copyWith(color: Colors.white), // Set text color to white
+                    'Gmail',
+                    style: TextStyle(
+                      color: Colors.white, fontSize: 18,
+                    ),
                   ),
                   onTap: () {
-                    // Handle Settings navigation here
+                    launch('https://mail.google.com/mail/u/0/#inbox');
                   },
                 ),
-                // Add more ListTile items for other navigation options with white text color
+                ListTile(
+                  leading: Icon(
+                    Icons.link,
+                    color: Colors.tealAccent,
+                  ),
+                  title: Text(
+                    'LinkedIn',
+                    style: TextStyle(
+                      color: Colors.white, fontSize: 18,
+                    ),
+                  ),
+                  onTap: () {
+                    launch('https://www.linkedin.com/in/wahidsabbir/');
+                  },
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.facebook,
+                    color: Colors.tealAccent,
+                  ),
+                  title: Text(
+                    'Facebook',
+                    style: TextStyle(
+                      color: Colors.white, fontSize: 18,
+                    ),
+                  ),
+                  onTap: () {
+                    launch('https://www.facebook.com/w.sabbir.p');
+                  },
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.camera_enhance_outlined,
+                    color: Colors.tealAccent,
+                  ),
+                  title: Text(
+                    'Instagram',
+                    style: TextStyle(
+                      color: Colors.white, fontSize: 18,
+                    ),
+                  ),
+                  onTap: () {
+                    launch('https://www.instagram.com/w.sabbir.p/');
+                  },
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.account_tree_sharp,
+                    color: Colors.tealAccent,
+                  ),
+                  title: Text(
+                    'Github',
+                    style: TextStyle(
+                      color: Colors.white, fontSize: 18,
+                    ),
+                  ),
+                  onTap: () {
+                    launch('https://github.com/w-sabbir-p');
+                  },
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.note_alt_sharp,
+                    color: Colors.tealAccent,
+                  ),
+                  title: Text(
+                    'Get CV',
+                    style: TextStyle(
+                      color: Colors.white, fontSize: 18,
+                    ),
+                  ),
+                  onTap: () {
+                    launch('https://drive.google.com/file/d/17MmjgrpJe5J0ekwBRC1XscZ-lnEhb0LK/view?usp=drivesdk');
+                  },
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.logout,
+                    color: Colors.tealAccent,
+                  ),
+                  title: Text(
+                    'Logout',
+                    style: TextStyle(
+                      color: Colors.white, fontSize: 18,
+                    ),
+                  ),
+                  onTap: () {
+                  },
+                ),
               ],
             ),
           ),
@@ -149,4 +265,13 @@ class HomeActivity extends StatelessWidget {
       ),
     );
   }
+
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw "Could not launch $url";
+    }
+  }
 }
+
